b8875740ce5b26eba08aa33484e5f8173cd66c94
Lee Xiang Wei <xiangweilee@seekasia.com>
2018-11-24 14:08:18 +0800

    Initial commit

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,32 @@
+import React, { Component, Fragment } from 'react'
+
+import classes from './Modal.module.css';
+import Backdrop from '../Backdrop/Backdrop';
+
+class Model extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  componentWillUpdate() {
+    console.log('[Model] WillUpdate');
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default Model;
---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,37 @@
+import React, { Component, Fragment } from 'react';
+
+import classes from './Layout.module.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  }
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer 
+          open={this.state.showSideDrawer} 
+          closed={this.sideDrawerClosedHandler} />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Fragment>
+    )
+  }
+} 
+
+export default Layout;
